4c2f59dca91e20faf385e21cbc10eec59996488f
elielton90 <elieltontorres@yahoo.com>
2016-07-19 17:54:23 -0300

    Add files via upload

---
+++ brincando_com_fogo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void valor();
+int main()
+
+{
+valor();
+
+}// O bloco termina aqui.}
+void valor(){
+for (int cont = 1; cont <= 100; cont++){
+if (cont % 2 == 0) cout << cont << endl;
+}
+}
+
